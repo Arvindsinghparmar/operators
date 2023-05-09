06c9b71ce739db55d8352900a9568995e1d3d2d2
Arvind Singh Parmar <Arvind Singh Parmar@DESKTOP-BG5A56S>
2023-05-09 06:08:15 +0530

    Add project files.

---
+++ operators/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            double num1=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Second number : ");
+            double num2=Convert.ToDouble(Console.ReadLine());
+            string op = ("+,-,*,/");
+            Console.WriteLine("Enter operator: "+ op);
+            op = Console.ReadLine();
+           
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1+num2);
+            }
+            else if(op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+
+            }
+            else if( op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+
+        }
+    }
+}
